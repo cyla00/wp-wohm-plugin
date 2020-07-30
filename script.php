f781a6b5865e4ef02ae9a675281858458d8844d1
cyla00 <ikhayam000@protonmail.com>
2020-07-30 23:01:13 +0200

    author, descr, name

--- script.php
+++ script.php
@@ -1,8 +1,10 @@
 <?php
 
-  /**
-  * Plugin Name: WOHM
-  * Description: melodiamalodika
+  /*
+  Plugin Name: WOHM
+  Description: |♠️|seroktikamelokdika|♠️|, ||Tutti pensano a cambiare il mondo, ma nessuno pensa a cambiar se stesso||🃏.
+  Author: Cyla00
+  Author URI: https://github.com/cyla00
   */
 
   echo '<script>
